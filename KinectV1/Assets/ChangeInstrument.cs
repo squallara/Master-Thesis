ee349f39c1c23851215508125c4bf439ed00e93d
Alexandros Panagiotis Giakalis <agiaka15@student.aau.dk>
2017-02-22 11:44:48 +0100

    Change interfaces

---
+++ KinectV1/Assets/ChangeInstrument.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeInstrument : MonoBehaviour {
+
+    public GameObject drums, synth;
+
+
+    void OnTriggerEnter()
+    {
+        if (Gameplay.instance.changed == false)
+        {
+            Gameplay.instance.changed = true;
+            if (gameObject.transform.IsChildOf(drums.transform))
+            {
+                drums.SetActive(false);
+                synth.SetActive(true);
+            }
+            else
+            {
+                drums.SetActive(true);
+                synth.SetActive(false);
+            }
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        Gameplay.instance.changed = false;
+    }
+}
---
+++ KinectV1/Assets/Gameplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour {
+
+    public static Gameplay instance;
+
+    [HideInInspector]
+    public bool changed;
+
+    void Start()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        changed = false;
+    }
+}
